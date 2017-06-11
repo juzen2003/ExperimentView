5a36dd56e38bebb598dd5869e2b8a1e873fe750e
Yu-Jen Chang <juzen2003@gmail.com>
2017-06-11 00:39:44 -0700

    0.0.0.1 experiment with image picker view, activity view, and alert view

--- ModalViewExperiment/ViewController.swift
+++ ModalViewExperiment/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Yu-Jen Chang on 6/10/17.
 //  Copyright © 2017 Yu-Jen Chang. All rights reserved.
 //
+//  test out a button to lauch image picker view
 
 import UIKit
 
@@ -15,11 +16,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // image picker view
+    @IBAction func presentImagePicker(_ sender: Any) {
+        let imagePickerView = UIImagePickerController()
+        self.present(imagePickerView, animated: true, completion: nil)
     }
-
-
+    
+    // activity view
+    @IBAction func presentActivityView(_ sender: Any) {
+        let image = UIImage()
+        let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(activityVC, animated: true, completion: nil)
+    }
+    
+    // alert view
+    @IBAction func presentAlertView(_ sender: Any) {
+        let alertVC = UIAlertController(title: "Alert Test", message: "This is a test", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action in self.dismiss(animated: true, completion: nil)})
+        alertVC.addAction(okAction)
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
 }
 
